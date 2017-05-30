d3cee6b3c51f352d79a82958e0b98c6e25616783
Ovidiu <Ovidiu@LAPTOP-MJRNSH14>
2017-05-30 16:30:43 +0300

    Add project files.

---
+++ MVCLoginRegistration/Models/UserAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCLoginRegistration.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage ="First name is required.")]
+        public string FirstName { get; set; }
+        
+        [Required(ErrorMessage ="Last name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage ="Email is required")]
+        [RegularExpression(@"^[\w!#$%&'*+\-/=?\^_`{|}~]+(\.[\w!#$%&'*+\-/=?\^_`{|}~]+)*"+ "@" + @"((([\-\w]+\.)+[a-zA-Z]{2,4})|(([0-9]{1,3}\.){3}[0-9]{1,3}))$", ErrorMessage ="Please enter a valid Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="User Name is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password",ErrorMessage ="Please confirm your password.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
